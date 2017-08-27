ef0d8d3d57a2145166719b54c10c3ecbfa21911b
William Kamp <manimaul@gmail.com>
2017-08-27 13:06:52 -0700

    adding google map view

--- app/src/main/java/io/madrona/geotimer/GeoFenceMap.kt
+++ app/src/main/java/io/madrona/geotimer/GeoFenceMap.kt
@@ -1,13 +1,68 @@
 package io.madrona.geotimer
 
 import android.app.Activity
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import io.madrona.geotimer.databinding.ActivityGeoFenceMapBinding
 
 class GeoFenceMap : Activity() {
 
+    var map: GoogleMap? = null
+    set(value) {
+        field = value
+        if (value != null) {
+            setupMap(value)
+        }
+    }
+
+    lateinit var viewBinding: ActivityGeoFenceMapBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_geo_fence_map)
+        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_geo_fence_map)
+        viewBinding.mapView.onCreate(savedInstanceState)
+        viewBinding.mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewBinding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewBinding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewBinding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        viewBinding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewBinding.mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        viewBinding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        viewBinding.mapView.onLowMemory()
+    }
+
+    private fun setupMap(map: GoogleMap) {
+
     }
 }
