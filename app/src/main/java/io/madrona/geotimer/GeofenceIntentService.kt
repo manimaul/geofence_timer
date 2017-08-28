34e9a9aca988d4823b9ef8b513ccf159926769f6
William Kamp <manimaul@gmail.com>
2017-08-28 06:12:23 -0700

    adding intent service to handle geofence events

---
+++ app/src/main/java/io/madrona/geotimer/GeofenceIntentService.kt
@@ -0,0 +1,30 @@
+package io.madrona.geotimer
+
+import android.app.IntentService
+import android.content.Intent
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
+
+private const val name = "MdrGeofenceService"
+
+class GeofenceIntentService: IntentService(name) {
+    override fun onHandleIntent(intent: Intent) {
+        val geoFenceEvent = GeofencingEvent.fromIntent(intent)
+        if (geoFenceEvent.hasError()) {
+            return
+        }
+        when (geoFenceEvent.geofenceTransition) {
+            Geofence.GEOFENCE_TRANSITION_ENTER -> enterFences(geoFenceEvent.triggeringGeofences)
+            Geofence.GEOFENCE_TRANSITION_EXIT -> exitFences(geoFenceEvent.triggeringGeofences)
+            Geofence.GEOFENCE_TRANSITION_DWELL -> {}
+        }
+    }
+
+    private fun enterFences(fences: List<Geofence>) {
+        val ids = fences.map { it.requestId }
+    }
+
+    private fun exitFences(fences: List<Geofence>) {
+        val ids = fences.map { it.requestId }
+    }
+}
